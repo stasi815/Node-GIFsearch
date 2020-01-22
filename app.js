2206b8eae146f7edd0a391a64073b233d0dc7d87
stasi815 <anastasia.gallardo@students.makeschool.com>
2020-01-22 15:59:07 -0800

    added search form

--- app.js
+++ app.js
@@ -5,18 +5,34 @@ const express = require('express');
 const app = express();
 
 // Middleware
+const exphbs  = require('express-handlebars');
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 // Routes
-app.get('/', (req, res) => {
-    res.send('Hello Squirrel');
-  });
-  
-  res.send('Hello Squirrel');
-});
+// app.get('/', (req, res) => {
+//     // set the url of the gif
+//     const gifUrl = 'https://media1.tenor.com/images/561c988433b8d71d378c9ccb4b719b6c/tenor.gif?itemid=10058245'
+//     // render the hello-gif view, passing the gifUrl into the view to be displayed
+//     res.render('hello-gif', { gifUrl })
+//     })
 
+
+app.get('/', (req, res) => {
+    res.render('home')
+    console.log(req.query) // => "{ term: hey" }
+
+  })
+
+  app.get('/greetings/:name', (req, res) => {
+// grab the name from the path provided
+const name = req.params.name;
+// render the greetings view, passing along the name
+res.render('greetings', { name });
+})
 // Start Server
 
 app.listen(3000, () => {
   console.log('Gif Search listening on port localhost:3000!');
-});
 });
